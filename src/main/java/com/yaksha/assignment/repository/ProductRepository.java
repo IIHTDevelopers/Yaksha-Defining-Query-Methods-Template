1d72a404aa2e112a776c4019b240c531f64901d9
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-04-20 17:27:30 +0530

    Update ProductRepository.java

--- src/main/java/com/yaksha/assignment/repository/ProductRepository.java
+++ src/main/java/com/yaksha/assignment/repository/ProductRepository.java
@@ -1,17 +1,11 @@
 package com.yaksha.assignment.repository;
 
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import com.yaksha.assignment.entity.Product;
 
-@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 	// Custom query method to find products by name
-	List<Product> findByName(String name);
 
 	// Custom query method to find products by price range
-	List<Product> findByPriceBetween(Double minPrice, Double maxPrice);
 }
